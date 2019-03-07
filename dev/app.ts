0d581750696aa706789ed820036f1ee43c4829bf
Jakub Chmielewski <jakub.chmielewski.89@gmail.com>
2019-03-07 04:56:09 +0100

    Acquiring manager, injector and registrator

--- dev/app.ts
+++ dev/app.ts
@@ -1,5 +1,7 @@
-import {Injector} from '../src';
+import {getInjectorManager} from "../src/injector/injector-manager";
 
 console.log('WAITS development app running');
 
-const injector = Injector.getInstance();
+const manager = getInjectorManager();
+const injector = manager.getInjector();
+const registrator = manager.getRegistrator();
