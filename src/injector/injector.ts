67a4e45c51d8af6075e6503fe7bd19ce40921ca5
Jakub Chmielewski <jakub.chmielewski.89@gmail.com>
2019-03-10 15:35:40 +0100

    Injector using Instantiator, returns Promise of the root class.

--- src/injector/injector.ts
+++ src/injector/injector.ts
@@ -1,11 +1,28 @@
-import {getRegistrator} from "./registration/registrator";
+import {Class} from "./type/classes";
+import {Instantiator} from "./instantiation/instantiator";
 
 export class Injector {
 
 
-  private _registrator = getRegistrator();
+  private _instanceMap: Instantiator.ClassNameToInstanceMap;
 
-  inject() {
-    // TODO
+
+  constructor() {
+    this._instanceMap = new Map<string, any>();
+  }
+
+
+  injectFor<T>(rootClass: Class<T>): Promise<T> {
+    const instantiator = new Instantiator<T>(rootClass);
+    this._instanceMap = instantiator.getInstanceMap();
+    return instantiator
+      .resolveOrder()
+      .instantiate()
+      .fire();
+  }
+
+
+  getInstance<T>(klass: Class<T>): T | undefined {
+    return this._instanceMap.get(klass.name);
   }
 }
