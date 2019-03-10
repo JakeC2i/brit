d20e1165258c8bf75de909b8c9e6a3761801500c
Jakub Chmielewski <jakub.chmielewski.89@gmail.com>
2019-03-10 22:20:24 +0100

    Async class readiness test.

--- dev/app.ts
+++ dev/app.ts
@@ -1,14 +1,29 @@
 import {getInjectorManager, Injectable} from "../src";
 
-@Injectable() class Roof {}
-@Injectable() class Walls {}
+@Injectable({async: true})
+class Roof {
+  constructor() {
+    console.log('Roof constructed synchronously, but not ready yet');
+    setTimeout(() => {
+      console.log('Roof asynchronously marked as ready');
+      Injectable.markReady(Roof);
+    }, 2000);
+  }
+}
+@Injectable() class Walls {
+  constructor() {
+    console.log('Walls constructed synchronously and ready');
+  }
+}
 
 @Injectable()
 class House {
   constructor(
     private _roof: Roof,
     private _walls: Walls
-  ) {}
+  ) {
+    console.log('House synchronously constructed and ready');
+  }
 }
 
 const injector = getInjectorManager().getInjector();
